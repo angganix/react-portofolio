6d81a5c04491a614130c856271325fee2ab63c06
Angga Eko Pratama <angganix@gmail.com>
2020-11-09 18:58:43 +0700

    Add React to Webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="container container-fluid">
+            
+        </div>
+    )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    
-}
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  performance: {
+      hints: false,
+      maxEntrypointSize: 1000000,
+      maxAssetSize: 1000000
+  }
+};
